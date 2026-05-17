86af7c311898428484351163ef81914b17c34b25
huangxiong1992 <1033407319@qq.com>
2026-05-17 23:10:38 +0800

    Update teng.js

--- teng.js
+++ teng.js
@@ -739,7 +739,7 @@ var rule = {
         let zp = d.filter(function (it) {
             return !(it.type && it.type !== "正片")
         });
-        VOD.vod_play_from = yg.length < 1 ? "qq" : "qq$$$qq 预告及花絮";
+        VOD.vod_play_from = yg.length < 1 ? "4K原画" : "qq$$$qq 预告及花絮";
         VOD.vod_play_url = yg.length < 1 ? d.map(function (it) {
             return it.title + "$" + it.url
         }).join("#") : [zp, yg].map(function (it) {
